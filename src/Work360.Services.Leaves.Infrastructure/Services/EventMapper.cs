4f7c5c03c0f3bb85636b7c97cd60008ee3472144
kamil4921 <kamil.j4921@gmail.com>
2024-11-26 15:31:06 +0100

    Added mapping events

---
+++ src/Work360.Services.Leaves.Infrastructure/Services/EventMapper.cs
@@ -0,0 +1,22 @@
+using MediatR;
+using Work360.Services.Leaves.Application.Services;
+using Work360.Services.Leaves.Core;
+using Work360.Services.Leaves.Core.Events;
+
+namespace Work360.Services.Leaves.Infrastructure.Services;
+
+public class EventMapper : IEventMapper
+{
+    public INotification? Map(IDomainEvent @event)
+    {
+        return @event switch
+        {
+            EmployeeCreated e => new Application.Events.EmployeeCreated(e.Employee.Id),
+            LeaveCreated e => new Application.Events.LeaveCreated(e.LeaveApplication.Id),
+            _ => null
+        };
+    }
+
+    public IEnumerable<INotification?> MapAll(IEnumerable<IDomainEvent> events)
+        => events.Select(Map);
+}
