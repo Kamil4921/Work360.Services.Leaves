d571a7bef68f59ead7c160b10ae07fffe79226d4
kamil4921 <kamil.j4921@gmail.com>
2024-12-12 19:53:14 +0100

    Changes to asynchronous communication to clean up the code

--- src/Work360.Services.Leaves.Infrastructure/Services/ServiceBusMessageReceiver.cs
+++ src/Work360.Services.Leaves.Infrastructure/Services/ServiceBusMessageReceiver.cs
@@ -1,40 +1,40 @@
 using Azure.Messaging.ServiceBus;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Work360.Services.Leaves.Application.Commands;
 using Work360.Services.Leaves.Application.DTO;
 
 namespace Work360.Services.Leaves.Infrastructure.Services;
 
-public class ServiceBusMessageReceiver (ISender mediator)
+public class ServiceBusMessageReceiver (ISender mediator, ILogger<ServiceBusMessageReceiver> logger)
 {
-    private const string connectionString = "Endpoint=sb://localhost:5672/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RootManageSharedAccessKeyValue;UseDevelopmentEmulator=true;";
-    private const string topicName = "employee-topic";
-    private const string subscriptionName = "subscription.3";
-    public ServiceBusProcessor _processor;
+    private const string ConnectionString = "Endpoint=sb://localhost:5672/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RootManageSharedAccessKeyValue;UseDevelopmentEmulator=true;";
+    private const string TopicName = "employee-topic";
+    private const string SubscriptionName = "subscription.3";
 
     public async Task StartAsync()
     {
-        var client = new ServiceBusClient(connectionString);
-        _processor = client.CreateProcessor(topicName, subscriptionName, new ServiceBusProcessorOptions());
+        var client = new ServiceBusClient(ConnectionString);
+        var processor = client.CreateProcessor(TopicName, SubscriptionName, new ServiceBusProcessorOptions());
 
-        _processor.ProcessMessageAsync += MessageHandler;
-        _processor.ProcessErrorAsync += ErrorHandler;
-        Console.WriteLine("starting service bus");
-        await _processor.StartProcessingAsync();
+        processor.ProcessMessageAsync += MessageHandler;
+        processor.ProcessErrorAsync += ErrorHandler;
+        logger.LogInformation("Starting service bus");
+        await processor.StartProcessingAsync();
     }
 
     private async Task MessageHandler(ProcessMessageEventArgs args)
     {
         var employees = JsonConvert.DeserializeObject<List<EmployeeDto>>(args.Message.Body.ToString())?? throw new ArgumentNullException();
-        var employee = employees!.First();
+        var employee = employees.First();
         await mediator.Send(new CreateEmployee(employee.EmployeeId));
     }
 
     private Task ErrorHandler(ProcessErrorEventArgs args)
     {
-        Console.WriteLine("error occured in the topic");
-        // dodac handlowanie errorow
+        logger.LogError("Error occured in the topic");
+        //TODO:Add monitoring
         return Task.CompletedTask;
     }
 }
