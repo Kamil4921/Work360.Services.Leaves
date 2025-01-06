ae2611572afba71802c8ca8d0992e55221d2b73d
kamil4921 <kamil.j4921@gmail.com>
2025-01-06 17:34:32 +0100

    Fix publishing messages

--- src/Work360.Services.Leaves.Application/Services/IMessageBroker.cs
+++ src/Work360.Services.Leaves.Application/Services/IMessageBroker.cs
@@ -4,6 +4,6 @@ namespace Work360.Services.Leaves.Application.Services;
 
 public interface IMessageBroker
 {
-    Task PublishAsync(params INotification[] events);
+    Task PublishAsync(INotification events);
     Task PublishAsync(IEnumerable<INotification> events);
 }
--- src/Work360.Services.Leaves.Infrastructure/Services/MessageBroker.cs
+++ src/Work360.Services.Leaves.Infrastructure/Services/MessageBroker.cs
@@ -1,19 +1,17 @@
-using System.Text.Json;
 using Azure.Messaging.ServiceBus;
 using MediatR;
 using Work360.Services.Leaves.Application.Services;
-using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Work360.Services.Leaves.Infrastructure.Services;
 
 public class MessageBroker : IMessageBroker
 {
     private const string connectionString = "Endpoint=sb://localhost:5672/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RootManageSharedAccessKeyValue;UseDevelopmentEmulator=true;";
-    public async Task PublishAsync(params INotification[] events)
+    public async Task PublishAsync(INotification events)
     {
         var client = new ServiceBusClient(connectionString);
         var sender = client.CreateSender("employee-topic");
-        var message = new ServiceBusMessage(JsonSerializer.Serialize(events, new JsonSerializerOptions{ WriteIndented = true }));
+        var message = new ServiceBusMessage(Newtonsoft.Json.JsonConvert.SerializeObject(events));
 
         await sender.SendMessageAsync(message);
     }
